b07b41fc5921b2f527cb9e883a764c1c52f26956
antny_gordo <anthonylgordon25@gmail.com>
2023-10-20 16:19:13 -0400

    update title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Task Master</title>
+    <title>The Social Machine</title>
     @vite('resources/css/app.css')
     <!-- Fonts -->
 </head>
