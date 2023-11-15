d02c6e180c43669e3c3caecb98c696d283a1bf81
antny_gordo <anthonylgordon25@gmail.com>
2023-11-15 14:54:50 -0500

    update collision and behavior

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-screen pb-10 overflow-y-auto">
+    <div class="h-screen overflow-y-auto">
         <div class="flex px-8 py-2 bg-primary overflow-y-auto">
             <h1 class="text-5xl font-bold text-white">The Social Machine</h1>
         </div>
--- resources/js/views/bubbles/Test.vue
+++ resources/js/views/bubbles/Test.vue
@@ -11,11 +11,11 @@ export default defineComponent({
         onMounted(async () => {
             await nextTick();
             if (containerRef.value) {
-                const width = 1000;
-                const height = 800;
+                const width = containerRef.value.clientWidth;
+                const height = containerRef.value.clientHeight;
                 const data = {
                     children: [
-                        { name: 'A', group: 1, value: 1 },
+                        { name: "New York<br>Vs<br>Miami", group: 1, value: 1 },
                         { name: 'B', group: 2, value: 1 },
                         { name: 'C', group: 1, value: 1 },
                         { name: 'D', group: 1, value: 1 },
@@ -42,15 +42,15 @@ export default defineComponent({
                 const hierarchy = d3.hierarchy(data).sum((d) => d.value);
                 const packLayout = d3.pack().size([width, height]).padding(1.5);
                 const nodes = packLayout(hierarchy).leaves();
-                const fixedRadius = 20;
+                const fixedRadius = 40;
 
                 simulation = d3
                     .forceSimulation(nodes)
-                    .force('x', d3.forceX(width / 2).strength(0.01))
-                    .force('y', d3.forceY(height / 2).strength(0.01))
-                    .force('cluster', forceCluster())
-                    .force('collide', forceCollide().radius((d) => fixedRadius).strength(1).iterations(1));
-
+                    // .force('x', d3.forceX(width / 2).strength(0.01))
+                    // .force('y', d3.forceY(height / 2).strength(0.01))
+                    .force('cluster', null)
+                    // .force('move', d3.forceManyBody().strength(5).distanceMax(200).distanceMin(50))
+                    .force('collide', customForceCollide().radius((d) => 20).strength(0.2));
                 const svg = d3.select(containerRef.value).append('svg').attr('width', width).attr('height', height);
 
                 const nodesGroup = svg
@@ -65,7 +65,15 @@ export default defineComponent({
                     .attr('cy', (d) => d.y)
                     .attr('r', 0) // Start with radius 0 for initial animation
                     .attr('fill', (d) => color(d.data.group))
+                    .attr('opacity', 0.5)
                     .call(d3.drag().on('start', dragstart).on('drag', dragged).on('end', dragend));
+
+                // nodes.forEach((node) => {
+                //     node.x = Math.random() * width;
+                //     node.y = Math.random() * height;
+                //     node.vx = Math.random() * 2 - 1; // Random x velocity between -1 and 1
+                //     node.vy = Math.random() * 2 - 1; //
+                // });
 
                 const textNode = nodesGroup
                     .append('text')
@@ -73,7 +81,19 @@ export default defineComponent({
                     .attr('y', (d) => d.y)
                     .attr('dy', '0.3em')
                     .attr('text-anchor', 'middle')
-                    .text((d) => d.data.name)
+                    .each(function(d) {
+                        const textLines = d.data.name.split('<br>');
+                        let currNode = this;
+                        let lineVal = (textLines.length > 1 ? -13 : 0);
+                        textLines.forEach(function(line, index) {
+                            d3.select(currNode)
+                                .append('tspan')
+                                .attr('x', 0)
+                                .attr('y', lineVal)
+                                .text(line);
+                            lineVal = lineVal + 18;
+                        });
+                    })
                     .call(d3.drag().on('start', textDragstart).on('drag', textDragged).on('end', textDragend));
 
                 node
@@ -86,8 +106,54 @@ export default defineComponent({
                     if (!event.active) simulation.alphaTarget(0.3).restart();
                     d.fx = d.x;
                     d.fy = d.y;
+                    d.dragStartX = event.x;
+                    d.dragStartY = event.y;
                 }
+                function customForceCollide() {
+                    let nodes;
+                    let strength = 0.2;
+                    let radius = d => 1;
 
+                    function force(alpha) {
+                        const quadtree = d3.quadtree(nodes, d => d.x, d => d.y);
+
+                        nodes.forEach(node => {
+                            const r = radius(node) + fixedRadius;
+                            const nx1 = node.x - r, ny1 = node.y - r;
+                            const nx2 = node.x + r, ny2 = node.y + r;
+
+                            quadtree.visit((quad, x1, y1, x2, y2) => {
+                                if (!quad.length) {
+                                    do {
+                                        const other = quad.data;
+                                        if (other && (other !== node)) {
+                                            let x = node.x - other.x;
+                                            let y = node.y - other.y;
+                                            let distance = Math.sqrt(x * x + y * y);
+                                            const minDistance = r + radius(other);
+
+                                            if (distance < minDistance) {
+                                                distance = (distance - minDistance) / distance * alpha * strength;
+                                                node.x -= x *= distance;
+                                                node.y -= y *= distance;
+                                                other.x += x;
+                                                other.y += y;
+                                            }
+                                        }
+                                    } while (quad = quad.next);
+                                }
+                                return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+                            });
+                        });
+                    }
+
+                    force.initialize = _ => nodes = _;
+
+                    force.strength = _ => (_ === undefined ? strength : (strength = +_, force));
+                    force.radius = _ => (_ === undefined ? radius : (radius = typeof _ === 'function' ? _ : constant(+_), force));
+
+                    return force;
+                }
                 function dragged(event, d) {
                     d.fx = event.x;
                     d.fy = event.y;
@@ -103,12 +169,18 @@ export default defineComponent({
                     if (!event.active) simulation.alphaTarget(0);
                     d.fx = null;
                     d.fy = null;
+                    // Calculate the angle between drag start and end positions
+                    const dx = event.x - d.dragStartX;
+                    const dy = event.y - d.dragStartY;
+                    d.initialAngle = Math.atan2(dy, dx);
                 }
 
                 function textDragstart(event, d) {
                     if (!event.active) simulation.alphaTarget(0.3).restart();
                     d.fx = d.x;
                     d.fy = d.y;
+                    d.dragStartX = event.x;
+                    d.dragStartY = event.y;
                 }
 
                 function textDragged(event, d) {
@@ -126,11 +198,65 @@ export default defineComponent({
                     if (!event.active) simulation.alphaTarget(0);
                     d.fx = null;
                     d.fy = null;
+                    // Calculate the angle between drag start and end positions
+                    const dx = event.x - d.dragStartX;
+                    const dy = event.y - d.dragStartY;
+                    d.initialAngle = Math.atan2(dy, dx);
                 }
 
+                nodes.forEach((node) => {
+                    node.x = Math.random() * width;
+                    node.y = Math.random() * height;
+                    node.initialAngle = Math.random() * 2 * Math.PI; // Store the initial angle
+                    node.vx = Math.cos(node.initialAngle);
+                    node.vy = Math.sin(node.initialAngle);
+                });
+
+                simulation.alphaDecay(0);
+                const angle = Math.random() * 2 * Math.PI;
                 simulation.on('tick', () => {
+                    const speed = 0.2;
+                    nodes.forEach((node) => {
+                        // Update position based on the initial angle
+                        node.x += speed * Math.cos(node.initialAngle);
+                        node.y += speed * Math.sin(node.initialAngle);
+
+                        // Bounce off the walls
+                        if (node.x - fixedRadius < 0 || node.x + fixedRadius > width) {
+                            node.initialAngle = Math.PI - node.initialAngle; // Reverse angle
+                        }
+
+                        if (node.y - fixedRadius < 0 || node.y + fixedRadius > height) {
+                            node.initialAngle = -node.initialAngle; // Reverse angle
+                        }
+                        // Function to check if two nodes are colliding
+                        function areColliding(nodeA, nodeB) {
+                            const dx = nodeA.x - nodeB.x;
+                            const dy = nodeA.y - nodeB.y;
+                            const distance = Math.sqrt(dx * dx + dy * dy);
+                            return distance < 2 * fixedRadius; // Adjust the factor as needed
+                        }
+                        // Check for collisions with other nodes
+                        // nodes.forEach((otherNode, index) => {
+                        //     if (node !== otherNode) {
+                        //         const dx = node.x - otherNode.x;
+                        //         const dy = node.y - otherNode.y;
+                        //         const distance = Math.sqrt(dx * dx + dy * dy);
+                        //
+                        //         // Check for collisions with other nodes
+                        //         for (let i = 0; i < nodes.length; i++) {
+                        //             if (i !== index && areColliding(node, nodes[i])) {
+                        //                 // Reverse angles for both colliding nodes
+                        //                 nodes[i].initialAngle = Math.PI - nodes[i].initialAngle;
+                        //                 node.initialAngle = Math.PI - node.initialAngle;
+                        //             }
+                        //         }
+                        //     }
+                        // });
+                    });
+
                     node.attr('cx', (d) => d.x).attr('cy', (d) => d.y);
-                    textNode.attr('x', (d) => d.x).attr('y', (d) => d.y);
+                    textNode.attr('transform', (d) => `translate(${d.x},${d.y})`);
                 });
 
                 function getRndInteger(min, max) {
